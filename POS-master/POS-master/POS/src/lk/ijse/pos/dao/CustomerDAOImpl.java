4086eed8b240d9d904dd3a7021c9a10638d3437a
Sandaru293 <skithshan93@gmail.com>
2021-08-26 16:47:53 +0530

    Implement cutomerDAOImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/CustomerDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/CustomerDAOImpl.java
@@ -1,5 +1,60 @@
 package lk.ijse.pos.dao;
 
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+
 public class CustomerDAOImpl {
-    
+    public boolean addCustomer(Customer customer) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
+        pstm.setObject(1, customer.getcID());
+        pstm.setObject(2, customer.getName());
+        pstm.setObject(3, customer.getAddress());
+        pstm.setObject(4, 0);
+        return (pstm.executeUpdate()>0);
+    }
+
+    public boolean deleteCustomer(String id) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+        pstm.setObject(1, id);
+        return (pstm.executeUpdate()>0);
+
+    }
+
+    public boolean UpdateCustomer(Customer customer) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        pstm.setObject(1, customer.getcID());
+        pstm.setObject(2, customer.getName());
+        pstm.setObject(3, customer.getAddress());
+        return (pstm.executeUpdate()>0);
+
+    }
+
+    public Customer searchCustomer(String id) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer where id=?");
+        if (rst.next()){
+            return new Customer(rst.getString("id"),rst.getString("name"),rst.getString("address"));
+        }
+    }
+
+    public ArrayList<Customer> getAllCustomers() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        ArrayList<CustomerTM> alCustomers = new ArrayList<>();
+        while (rst.next()){
+            Customer customer= new Customer(rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3));
+            alCustomers.add(customer);
+
+        }
+        return alCustomers;
+    }
 }
