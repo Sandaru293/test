6132cf076b9d3149e62f3d7f048217a6ba799b48
Sandaru293 <skithshan93@gmail.com>
2021-08-27 07:57:38 +0530

    Add crudUtil

---
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/CrudUtil.java
@@ -0,0 +1,29 @@
+package lk.ijse.pos.dao;
+
+import lk.ijse.pos.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    private static PreparedStatement getPreparedStatement(String sql, Object... params) throws Exception, SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            pstm.setObject((i + 1), params[i]);
+        }
+        return pstm;
+    }
+
+    public static boolean executeUpdate(String sql, Object... params) throws Exception, SQLException {
+        PreparedStatement pstm = getPreparedStatement(sql, params);
+        return pstm.executeUpdate() > 0;
+    }
+
+    public static ResultSet executeQuery(String sql, Object... params) throws Exception, SQLException {
+        PreparedStatement pstm = getPreparedStatement(sql, params);
+        return pstm.executeQuery();
+    }
+}
