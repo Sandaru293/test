535ad9e034c6c2791ddc07fa9f089ec6b6d92fe2
Sandaru293 <skithshan93@gmail.com>
2021-08-27 08:03:12 +0530

    Modified itemDAOImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/ItemDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/ItemDAOImpl.java
@@ -1,62 +1,43 @@
 package lk.ijse.pos.dao;
 
+import lk.ijse.pos.db.DBConnection;
+import lk.ijse.pos.model.Item;
+
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class ItemDAOImpl {
-    public boolean addItem(Item item) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item VALUES (?,?,?,?)");
-        pstm.setObject(1, item.getCode());
-        pstm.setObject(2, item.getDescription());
-        pstm.setObject(3, new BigDecimal(item.getUnitPrice()));
-        pstm.setObject(4, Integer.parseInt(item.getQtyOnHand()));
-        return (pstm.executeUpdate()>0);
+    public boolean addItem(Item item) throws Exception {
+        String sql = "INSERT INTO Item VALUES (?,?,?,?)";
+        return CrudUtil.executeUpdate(sql,item.getCode(),item.getDescription(),new BigDecimal(String.valueOf(item.getUnitPrice())),item.getQtyOnHand());
     }
 
-    public boolean deleteItem(String id) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
-        pstm.setObject(1, id);
-        return (pstm.executeUpdate()>0);
-
+    public boolean deleteItem(String id) throws Exception {
+        String sql ="DELETE FROM Item WHERE code=?";
+        return CrudUtil.executeUpdate(sql, id);
     }
 
-    public boolean updateItem(Item item) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
-        pstm.setObject(1, item.getDescription());
-        pstm.setObject(2, new BigDecimal(item.getUnitPrice()));
-        pstm.setObject(3, Integer.parseInt(item.getQtyOnHand()));
-        pstm.setObject(4, item.getCode());
-        return (pstm.executeUpdate()>0);
-
+    public boolean updateItem(Item item) throws Exception {
+        String sql = "UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?";
+        return CrudUtil.executeUpdate(sql,item.getDescription(),new BigDecimal(String.valueOf(item.getUnitPrice())),item.getQtyOnHand(),item.getCode());
     }
 
-    public Item searchItem(String id) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM Item where code=?");
+    public Item searchItem(String id) throws Exception {
+        String sql = "SELECT * FROM Item where code=?";
+        ResultSet rst = CrudUtil.executeQuery(sql, id);
         if (rst.next()){
-            return new Item(rst.getString(1),
-                    rst.getBigDecimal(2),
-                    rst.getInt(3),
-                    rst.getString(4));
+            return new Item(rst.getString(1),rst.getBigDecimal(2), rst.getInt(3), rst.getString(4));
         }
         return null;
     }
 
-    public ArrayList<Item>getAllItems() throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+    public ArrayList<Item>getAllItems() throws Exception {
+        String sql = "SELECT * FROM Item";
+        ResultSet rst = CrudUtil.executeQuery(sql);
         ArrayList<Item> alItems = new ArrayList<>();
         while (rst.next()){
-            Item item= new Item(rst.getString(1),
-                    rst.getString(2),
-                    rst.getBigDecimal(3),
-                    rst.getInt(4));
+            Item item= new Item(rst.getString(1),rst.getString(2),rst.getBigDecimal(3),rst.getInt(4));
             alItems.add(item);
         }
         return alItems;
