fb6b5cf4c6ce027fd474723148989fe2a29c5e65
Sandaru293 <skithshan93@gmail.com>
2021-08-29 20:35:42 +0530

    Modified customerDAOImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/CustomerDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/CustomerDAOImpl.java
@@ -6,39 +6,107 @@ import lk.ijse.pos.model.Customer;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class CustomerDAOImpl {
+public class CustomerDAOImpl implements CustomerDAO{
+    @Override
     public boolean addCustomer(Customer customer) throws Exception {
-        String sql = "INSERT INTO Customer VALUES (?,?,?,?)";
-        return CrudUtil.executeUpdate(sql, customer.getcID(), customer.getName(), customer.getAddress());
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
+        pstm.setObject(1, customer.getcID());
+        pstm.setObject(2, customer.getName());
+        pstm.setObject(3, customer.getAddress());
+        pstm.setObject(4, 0);
+        return pstm.executeUpdate() > 0;
+//        String sql = "INSERT INTO Customer VALUES (?,?,?)";
+//        return CrudUtil.executeUpdate(sql, customer.getcID(),customer.getName(),customer.getAddress());
     }
 
+    @Override
     public boolean deleteCustomer(String id) throws Exception {
-        String sql ="DELETE FROM Customer WHERE cId=?";
+        String sql = "DELETE FROM Customer WHERE id=?";
         return CrudUtil.executeUpdate(sql, id);
     }
 
+    @Override
     public boolean updateCustomer(Customer customer) throws Exception {
-        String sql = "UPDATE Customer SET name=?, address=? WHERE cId=?";
-        return CrudUtil.executeUpdate(sql, customer.getName(), customer.getAddress(), customer.getcID());
+        String sql = "UPDATE Customer SET name=?, address=? WHERE id=?";
+        return CrudUtil.executeUpdate(sql, customer.getName(),customer.getAddress(),customer.getcID());
     }
 
+    @Override
     public Customer searchCustomer(String id) throws Exception {
-        String sql = "select * from Customer where cId=?";
+        String sql = "select * from Customer where id=?";
         ResultSet rst = CrudUtil.executeQuery(sql, id);
-        if (rst.next()){
-            return new Customer(rst.getString(1),rst.getString(2),rst.getString(3));
+        if (rst.next()) {
+            return new Customer(rst.getString(1), rst.getString(2), rst.getString(3));
         }
         return null;
     }
 
+    @Override
     public ArrayList<Customer> getAllCustomers() throws Exception {
         String sql = "SELECT * FROM Customer";
         ResultSet rst = CrudUtil.executeQuery(sql);
-        ArrayList<Customer> alCustomers = new ArrayList<>();
-        while (rst.next()){
-            Customer customer= new Customer(rst.getString(1),rst.getString(2),rst.getString(3));
-            alCustomers.add(customer);
+        ArrayList<Customer> allCustomers = new ArrayList<>();//CustomerTM
+        while (rst.next()) {
+            Customer customer =new Customer(rst.getString(1), rst.getString(2), rst.getString(3));
+            allCustomers.add(customer);
         }
-        return alCustomers;
+        return allCustomers;
     }
+
 }
+
+
+
+
+
+
+//    public boolean addCustomer(Customer customer) throws Exception {
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
+//        pstm.setObject(1, customer.getcID());
+//        pstm.setObject(2, customer.getName());
+//        pstm.setObject(3, customer.getAddress());
+//        pstm.setObject(4, 0);
+//        return pstm.executeUpdate() > 0;
+//    }
+//
+//    public boolean deleteCustomer(String id) throws Exception {
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+//        pstm.setObject(1, id);
+//        return pstm.executeUpdate() > 0;
+//    }
+//
+//    public boolean updateCustomer(Customer customer) throws Exception {
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+//        pstm.setObject(1, customer.getName());
+//        pstm.setObject(2, customer.getAddress());
+//        pstm.setObject(3, customer.getcID());
+//        return pstm.executeUpdate() > 0;
+//    }
+//
+//    public Customer searchCustomer(String id) throws Exception {
+//        String sql = "select * from Customer where id=?";
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement(sql);
+//        pstm.setObject(1, id);
+//        ResultSet rst = pstm.executeQuery();
+//        if (rst.next()) {
+//            return new Customer(rst.getString(1), rst.getString(2), rst.getString(3));
+//        }
+//        return null;
+//    }
+//
+//    public ArrayList<Customer> getAllCustomers() throws Exception {
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        Statement stm = connection.createStatement();
+//        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+//        ArrayList<Customer> allCustomers = new ArrayList<>();//CustomerTM
+//        while (rst.next()) {
+//            Customer customer =new Customer(rst.getString(1), rst.getString(2), rst.getString(3));
+//            allCustomers.add(customer);
+//        }
+//        return allCustomers;
+//    }
