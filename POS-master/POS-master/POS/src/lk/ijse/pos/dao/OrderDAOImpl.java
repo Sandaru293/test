5e6242de9e3af724ce2ea4e6191772dd46b170a4
Sandaru293 <skithshan93@gmail.com>
2021-08-29 20:43:52 +0530

    Modified orderDOAImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDAOImpl.java
@@ -3,44 +3,68 @@ package lk.ijse.pos.dao;
 import lk.ijse.pos.db.DBConnection;
 import lk.ijse.pos.model.Orders;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
-public class OrderDAOImpl {
+public class OrderDAOImpl implements OrderDAO{
+
+
+    @Override
     public boolean addOrder(Orders orders) throws Exception {
+        Connection connection = DBConnection.getInstance().getConnection();
         String sql = "INSERT INTO Orders VALUES (?,?,?)";
-        return CrudUtil.executeUpdate(sql,orders.getId(),orders.getDate(),orders.getCustomerId());
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setObject(1, orders.getId());
+        pstm.setObject(2, orders.getDate());
+        pstm.setObject(3, orders.getCustomerId());
+        return (pstm.executeUpdate() > 0);
     }
 
-    public boolean deleteOrder(String id) throws Exception {
-        String sql ="DELETE FROM Orders WHERE id=?";
-        return CrudUtil.executeUpdate(sql, id);
-
+    @Override
+    public boolean deleteOrder() throws Exception {
+        return false;
     }
 
-    public boolean updateOrder(Orders orders) throws Exception {
-        String sql = "UPDATE Orders SET date=?, customerId=? WHERE id=?";
-        return CrudUtil.executeUpdate(sql,orders.getDate(),orders.getCustomerId(),orders.getId());
+    @Override
+    public boolean updateOrder() throws Exception {
+        return false;
     }
 
-    public Orders searchOrder(String id) throws Exception {
-        String sql ="SELECT * FROM Orders where id=?";
-        ResultSet rst = CrudUtil.executeQuery(sql, id);
-        if (rst.next()){
-            return new Orders(rst.getString(1),rst.getDate(2),rst.getString(3));
-        }
+    @Override
+    public Orders searchOrder() throws Exception {
         return null;
     }
 
+    @Override
     public ArrayList<Orders> getAllOrders() throws Exception {
-        String sql = "SELECT * FROM Orders";
-        ResultSet rst = CrudUtil.executeQuery(sql);
-        ArrayList<Orders> allOrders = new ArrayList<>();
-        while (rst.next()) {
-            allOrders.add(new Orders(rst.getString(1), rst.getDate(2), rst.getString(3)));
-        }
-
-        return allOrders;
+        return null;
     }
-
 }
+
+
+//    public boolean addOrder(Orders orders) throws Exception {
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        String sql = "INSERT INTO Orders VALUES (?,?,?)";
+//        PreparedStatement pstm = connection.prepareStatement(sql);
+//        pstm.setObject(1, orders.getId());
+//        pstm.setObject(2, orders.getDate());
+//        pstm.setObject(3, orders.getCustomerId());
+//        return (pstm.executeUpdate() > 0);
+//    }
+//
+//    public boolean deleteOrder() {
+//        throw new UnsupportedOperationException("Not Implement");
+//    }
+//
+//    public boolean updateOrder() {
+//        throw new UnsupportedOperationException("Not Implement");
+//    }
+//
+//    public Orders searchOrder() {
+//        throw new UnsupportedOperationException("Not Implement");
+//    }
+//
+//    public ArrayList<Orders> getAllOrders() {
+//        throw new UnsupportedOperationException("Not Implement");
+//    }
