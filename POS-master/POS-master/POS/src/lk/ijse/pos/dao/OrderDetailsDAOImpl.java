2c5a54532f5bb9e7184ca2ee55498fee5596fd28
Sandaru293 <skithshan93@gmail.com>
2021-08-29 20:46:35 +0530

    Modified orderDetailsDAO

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDetailsDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDetailsDAOImpl.java
@@ -1,54 +1,32 @@
 package lk.ijse.pos.dao;
 
-import java.sql.*;
-import java.util.ArrayList;
+import lk.ijse.pos.db.DBConnection;
+import lk.ijse.pos.model.OrderDetails;
 
-public class OrderDetailsDAOImpl {
-    public boolean addOrderDetails(OrderDetails orderDetails) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO OrderDetails  VALUES (?,?,?,?)");
-        pstm.setObject(1, orderDetails.getOrderId());
-        pstm.setObject(2, orderDetails.getItemCode());
-        pstm.setObject(3, orderDetails.getQty());
-        pstm.setObject(4, orderDetails.getUnitPrice());
-        return (pstm.executeUpdate()>0);
-    }
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-    public boolean deleteOrderDetails(String id) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("DELETE FROM OrderDetails  WHERE orderId=?");
-        pstm.setObject(1, id);
-        return (pstm.executeUpdate()>0);
-    }
+public class OrderDetailsDAOImpl implements OrderDetailsDAO{
 
-    public boolean updateOrderDetails(OrderDetails orderDetails) throws SQLException {
+    @Override
+    public boolean addOrderDetails(OrderDetails oDetails) throws Exception {
         Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("UPDATE OrderDetails SET itemCode=?, qty=? , unitPrice=? WHERE orderId=?");
-        pstm.setObject(1, orderDetails.getItemCode());
-        pstm.setObject(2, orderDetails.getQty());
-        pstm.setObject(3, orderDetails.getUnitPrice());
-        pstm.setObject(4, orderDetails.getOrderId());
-        return (pstm.executeUpdate()>0);
-    }
-
-    public OrderDetails searchOrderDetail(String id) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM OrderDetails where orderId=?");
-        if (rst.next()){
-            return new OrderDetails(rst.getString(1),rst.getString(2),rst.getInt(3),rst.getBigDecimal(4));
-        }
-        return null;
-    }
-
-    public ArrayList<OrderDetails> getAllOrderDetails() throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM OrderDetails");
-        ArrayList<OrderDetails> allOrderDetails = new ArrayList<>();
-        while (rst.next()) {
-            allOrderDetails.add(new OrderDetails(rst.getString(1),rst.getString(2),rst.getInt(3),rst.getBigDecimal(4));
-        }
-        return allOrderDetails;
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO OrderDetails VALUES (?,?,?,?)");
+        pstm.setObject(1, oDetails.getOrderId());
+        pstm.setObject(2, oDetails.getItemCode());
+        pstm.setObject(3, oDetails.getQty());
+        pstm.setObject(4, oDetails.getUnitPrice());
+        return (pstm.executeUpdate() > 0);
     }
 }
+
+
+//    public boolean addOrderDetails(OrderDetails oDetails) throws Exception {
+//        Connection connection = DBConnection.getInstance().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("INSERT INTO OrderDetails VALUES (?,?,?,?)");
+//        pstm.setObject(1, oDetails.getOrderId());
+//        pstm.setObject(2, oDetails.getItemCode());
+//        pstm.setObject(3, oDetails.getQty());
+//        pstm.setObject(4, oDetails.getUnitPrice());
+//        return (pstm.executeUpdate() > 0);
+//    }
