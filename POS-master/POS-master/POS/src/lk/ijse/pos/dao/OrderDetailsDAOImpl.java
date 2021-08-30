1e4029df7a2b9c8a1c7e69d4fedf79f41adb16f6
Sandaru293 <skithshan93@gmail.com>
2021-08-30 14:39:24 +0530

    Modified orderDetailsDAOImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDetailsDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDetailsDAOImpl.java
@@ -1,22 +1,34 @@
 package lk.ijse.pos.dao;
 
-import lk.ijse.pos.db.DBConnection;
 import lk.ijse.pos.model.OrderDetails;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 public class OrderDetailsDAOImpl implements OrderDetailsDAO{
 
     @Override
-    public boolean addOrderDetails(OrderDetails oDetails) throws Exception {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO OrderDetails VALUES (?,?,?,?)");
-        pstm.setObject(1, oDetails.getOrderId());
-        pstm.setObject(2, oDetails.getItemCode());
-        pstm.setObject(3, oDetails.getQty());
-        pstm.setObject(4, oDetails.getUnitPrice());
-        return (pstm.executeUpdate() > 0);
+    public boolean add(OrderDetails oDetails) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO OrderDetails VALUES (?,?,?,?)", oDetails.getOrderId(),oDetails.getItemCode(),oDetails.getQty(),oDetails.getUnitPrice());
+    }
+
+    @Override
+    public boolean delete(String s) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDetails orderDetails) throws Exception {
+        return false;
+    }
+
+    @Override
+    public OrderDetails search(String s) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ArrayList<OrderDetails> getAll() throws Exception {
+        return null;
     }
 }
 
