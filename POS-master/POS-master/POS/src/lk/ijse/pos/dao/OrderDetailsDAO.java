02636d048f7017d214c85bb4aaab10d023c1152c
Sandaru293 <skithshan93@gmail.com>
2021-08-30 14:38:15 +0530

    Modified orderDeatailsDAO

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDetailsDAO.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDetailsDAO.java
@@ -2,6 +2,6 @@ package lk.ijse.pos.dao;
 
 import lk.ijse.pos.model.OrderDetails;
 
-public interface OrderDetailsDAO {
-    public boolean addOrderDetails(OrderDetails oDetails) throws Exception;
+public interface OrderDetailsDAO extends SuperDAO <OrderDetails, String>{
+    // public boolean addOrderDetails(OrderDetails oDetails) throws Exception;
 }
