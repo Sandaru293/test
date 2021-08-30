6feb0e6597f6d40cebe836472f6d40e88e76cc3f
Sandaru293 <skithshan93@gmail.com>
2021-08-30 14:34:28 +0530

    Modified itemDAOImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/ItemDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/ItemDAOImpl.java
@@ -1,32 +1,27 @@
 package lk.ijse.pos.dao;
 
-import lk.ijse.pos.db.DBConnection;
 import lk.ijse.pos.model.Item;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemDAOImpl implements ItemDAO{
 
     @Override
-    public boolean addItem(Item item) throws Exception {
-        String sql = "INSERT INTO Item VALUES (?,?,?,?)";
-        return CrudUtil.executeUpdate(sql, item.getCode(), item.getDescription(), item.getQtyOnHand(), item.getUnitPrice());
+    public boolean add(Item i) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO Item VALUES (?,?,?,?)", i.getCode(), i.getDescription(), i.getQtyOnHand(), i.getUnitPrice());
     }
 
     @Override
-    public boolean deleteItem(String code) throws Exception {
+    public boolean delete(String code) throws Exception {
         String sql = "DELETE FROM Item WHERE code=?";
         return CrudUtil.executeUpdate(sql, code);
     }
 
     @Override
-    public boolean updateItem(Item item) throws Exception {
+    public boolean update(Item i) throws Exception {
         String sql = "UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?";
-        return CrudUtil.executeUpdate(sql, item.getDescription(), item.getQtyOnHand(), item.getUnitPrice(), item.getCode());
+        return CrudUtil.executeUpdate(sql, i.getDescription(), i.getQtyOnHand(), i.getUnitPrice(), i.getCode());
     }
 
     @Override
@@ -36,7 +31,7 @@ public class ItemDAOImpl implements ItemDAO{
     }
 
     @Override
-    public Item searchItem(String code) throws Exception {
+    public Item search(String code) throws Exception {
         String sql = "select * from Item where code=?";
         ResultSet rst = CrudUtil.executeQuery(sql, code);
         if (rst.next()) {
@@ -46,7 +41,7 @@ public class ItemDAOImpl implements ItemDAO{
     }
 
     @Override
-    public ArrayList<Item> getAllItems() throws Exception {
+    public ArrayList<Item> getAll() throws Exception {
         String sql = "Select * from Item";
         ResultSet rst = CrudUtil.executeQuery(sql);
         ArrayList<Item> allItems = new ArrayList<>();
@@ -59,50 +54,3 @@ public class ItemDAOImpl implements ItemDAO{
         return allItems;
     }
 }
-
-
-
-//    public boolean addItem(Item item) throws Exception, SQLException {
-//        String sql = "INSERT INTO Item VALUES (?,?,?,?)";
-//        return CrudUtil.executeUpdate(sql, item.getCode(), item.getDescription(), item.getQtyOnHand(), item.getUnitPrice());
-//    }
-//
-//    public boolean deleteItem(String code) throws Exception {
-//        String sql = "DELETE FROM Item WHERE code=?";
-//        return CrudUtil.executeUpdate(sql, code);
-//    }
-//
-//    public boolean updateItem(Item item) throws Exception {
-//        String sql = "UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?";
-//        return CrudUtil.executeUpdate(sql, item.getDescription(), item.getQtyOnHand(), item.getUnitPrice(), item.getCode());
-//    }
-//
-//    public boolean updateItemQtyOnHand(String code,int qtyOnHand) throws Exception {
-//        Connection connection = DBConnection.getInstance().getConnection();
-//        PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET qtyOnHand=? WHERE code=?");
-//        pstm.setObject(1, qtyOnHand);
-//        pstm.setObject(2, code);
-//        return (pstm.executeUpdate() > 0);
-//    }
-//
-//    public Item searchItem(String code) throws Exception {
-//        String sql = "select * from Item where code=?";
-//        ResultSet rst = CrudUtil.executeQuery(sql, code);
-//        if (rst.next()) {
-//            return new Item(rst.getString(1),rst.getString(2),rst.getBigDecimal(3), rst.getInt(4));
-//        }
-//        return null;
-//    }
-//
-//    public ArrayList<Item> getAllItems() throws Exception {
-//        String sql = "Select * from Item";
-//        ResultSet rst = CrudUtil.executeQuery(sql);
-//        ArrayList<Item> allItems = new ArrayList<>();
-//        while (rst.next()) {
-//            allItems.add(new Item(rst.getString(1),
-//                    rst.getString(2),
-//                    rst.getBigDecimal(3),
-//                    rst.getInt(4)));
-//        }
-//        return allItems;
-//    }
