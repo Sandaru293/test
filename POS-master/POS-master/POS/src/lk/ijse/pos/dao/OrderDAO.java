2520970844d92d22f606a12acc99b8d5b6f84e1b
Sandaru293 <skithshan93@gmail.com>
2021-08-30 14:35:38 +0530

    Modified orderDAO

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDAO.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/OrderDAO.java
@@ -4,14 +4,14 @@ import lk.ijse.pos.model.Orders;
 
 import java.util.ArrayList;
 
-public interface OrderDAO {
-    public boolean addOrder(Orders orders) throws Exception;
-
-    public boolean deleteOrder()throws Exception;
-
-    public boolean updateOrder() throws Exception;
-
-    public Orders searchOrder() throws Exception;
-
-    public ArrayList<Orders> getAllOrders() throws Exception;
+public interface OrderDAO extends SuperDAO <Orders, String>{
+//    public boolean addOrder(Orders orders) throws Exception;
+//
+//    public boolean deleteOrder()throws Exception;
+//
+//    public boolean updateOrder() throws Exception;
+//
+//    public Orders searchOrder() throws Exception;
+//
+//    public ArrayList<Orders> getAllOrders() throws Exception;
 }
