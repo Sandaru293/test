13f70ecd50dfed6adbf664e978e64b7c6282a847
Sandaru293 <skithshan93@gmail.com>
2021-08-30 14:32:16 +0530

    Modified customerDAOImpl

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/CustomerDAOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/CustomerDAOImpl.java
@@ -1,6 +1,5 @@
 package lk.ijse.pos.dao;
 
-import lk.ijse.pos.db.DBConnection;
 import lk.ijse.pos.model.Customer;
 
 import java.sql.*;
@@ -8,32 +7,24 @@ import java.util.ArrayList;
 
 public class CustomerDAOImpl implements CustomerDAO{
     @Override
-    public boolean addCustomer(Customer customer) throws Exception {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
-        pstm.setObject(1, customer.getcID());
-        pstm.setObject(2, customer.getName());
-        pstm.setObject(3, customer.getAddress());
-        pstm.setObject(4, 0);
-        return pstm.executeUpdate() > 0;
-//        String sql = "INSERT INTO Customer VALUES (?,?,?)";
-//        return CrudUtil.executeUpdate(sql, customer.getcID(),customer.getName(),customer.getAddress());
+    public boolean add(Customer c) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO Customer VALUES (?,?,?,?)", c.getcID(),c.getName(),c.getAddress(),0);
     }
 
     @Override
-    public boolean deleteCustomer(String id) throws Exception {
+    public boolean delete(String id) throws Exception {
         String sql = "DELETE FROM Customer WHERE id=?";
         return CrudUtil.executeUpdate(sql, id);
     }
 
     @Override
-    public boolean updateCustomer(Customer customer) throws Exception {
+    public boolean update(Customer c) throws Exception {
         String sql = "UPDATE Customer SET name=?, address=? WHERE id=?";
-        return CrudUtil.executeUpdate(sql, customer.getName(),customer.getAddress(),customer.getcID());
+        return CrudUtil.executeUpdate(sql, c.getName(),c.getAddress(),c.getcID());
     }
 
     @Override
-    public Customer searchCustomer(String id) throws Exception {
+    public Customer search(String id) throws Exception {
         String sql = "select * from Customer where id=?";
         ResultSet rst = CrudUtil.executeQuery(sql, id);
         if (rst.next()) {
@@ -43,7 +34,7 @@ public class CustomerDAOImpl implements CustomerDAO{
     }
 
     @Override
-    public ArrayList<Customer> getAllCustomers() throws Exception {
+    public ArrayList<Customer> getAll() throws Exception {
         String sql = "SELECT * FROM Customer";
         ResultSet rst = CrudUtil.executeQuery(sql);
         ArrayList<Customer> allCustomers = new ArrayList<>();//CustomerTM
@@ -53,14 +44,7 @@ public class CustomerDAOImpl implements CustomerDAO{
         }
         return allCustomers;
     }
-
-}
-
-
-
-
-
-
+//    @Override
 //    public boolean addCustomer(Customer customer) throws Exception {
 //        Connection connection = DBConnection.getInstance().getConnection();
 //        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
@@ -69,40 +53,36 @@ public class CustomerDAOImpl implements CustomerDAO{
 //        pstm.setObject(3, customer.getAddress());
 //        pstm.setObject(4, 0);
 //        return pstm.executeUpdate() > 0;
+////        String sql = "INSERT INTO Customer VALUES (?,?,?)";
+////        return CrudUtil.executeUpdate(sql, customer.getcID(),customer.getName(),customer.getAddress());
 //    }
 //
+//    @Override
 //    public boolean deleteCustomer(String id) throws Exception {
-//        Connection connection = DBConnection.getInstance().getConnection();
-//        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
-//        pstm.setObject(1, id);
-//        return pstm.executeUpdate() > 0;
+//        String sql = "DELETE FROM Customer WHERE id=?";
+//        return CrudUtil.executeUpdate(sql, id);
 //    }
 //
+//    @Override
 //    public boolean updateCustomer(Customer customer) throws Exception {
-//        Connection connection = DBConnection.getInstance().getConnection();
-//        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
-//        pstm.setObject(1, customer.getName());
-//        pstm.setObject(2, customer.getAddress());
-//        pstm.setObject(3, customer.getcID());
-//        return pstm.executeUpdate() > 0;
+//        String sql = "UPDATE Customer SET name=?, address=? WHERE id=?";
+//        return CrudUtil.executeUpdate(sql, customer.getName(),customer.getAddress(),customer.getcID());
 //    }
 //
+//    @Override
 //    public Customer searchCustomer(String id) throws Exception {
 //        String sql = "select * from Customer where id=?";
-//        Connection connection = DBConnection.getInstance().getConnection();
-//        PreparedStatement pstm = connection.prepareStatement(sql);
-//        pstm.setObject(1, id);
-//        ResultSet rst = pstm.executeQuery();
+//        ResultSet rst = CrudUtil.executeQuery(sql, id);
 //        if (rst.next()) {
 //            return new Customer(rst.getString(1), rst.getString(2), rst.getString(3));
 //        }
 //        return null;
 //    }
 //
+//    @Override
 //    public ArrayList<Customer> getAllCustomers() throws Exception {
-//        Connection connection = DBConnection.getInstance().getConnection();
-//        Statement stm = connection.createStatement();
-//        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+//        String sql = "SELECT * FROM Customer";
+//        ResultSet rst = CrudUtil.executeQuery(sql);
 //        ArrayList<Customer> allCustomers = new ArrayList<>();//CustomerTM
 //        while (rst.next()) {
 //            Customer customer =new Customer(rst.getString(1), rst.getString(2), rst.getString(3));
@@ -110,3 +90,6 @@ public class CustomerDAOImpl implements CustomerDAO{
 //        }
 //        return allCustomers;
 //    }
+
+}
+
