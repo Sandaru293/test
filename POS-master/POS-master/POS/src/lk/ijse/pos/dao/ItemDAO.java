5df4e54d53a57dce0318eb7b7d960d3044ad762b
Sandaru293 <skithshan93@gmail.com>
2021-08-30 14:33:20 +0530

    Modified itemDAO

--- POS-master/POS-master/POS/src/lk/ijse/pos/dao/ItemDAO.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dao/ItemDAO.java
@@ -1,19 +1,10 @@
 package lk.ijse.pos.dao;
 
 import lk.ijse.pos.model.Item;
+;
 
-import java.util.ArrayList;
-
-public interface ItemDAO {
-    public boolean addItem(Item item) throws Exception;
-
-    public boolean deleteItem(String code) throws Exception;
-
-    public boolean updateItem(Item item) throws Exception;
+public interface ItemDAO extends SuperDAO <Item, String>{
 
     public boolean updateItemQtyOnHand(String code,int qtyOnHand) throws Exception;
 
-    public Item searchItem(String code) throws Exception;
-
-    public ArrayList<Item> getAllItems() throws Exception;
 }
