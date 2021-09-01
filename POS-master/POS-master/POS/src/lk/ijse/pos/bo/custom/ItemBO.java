bbf3b1fad366c0bf9856a03d78c5a595269e80ac
Sandaru293 <skithshan93@gmail.com>
2021-09-01 18:10:05 +0530

    Modified itemBO

--- POS-master/POS-master/POS/src/lk/ijse/pos/bo/custom/ItemBO.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/bo/custom/ItemBO.java
@@ -1,20 +1,21 @@
 package lk.ijse.pos.bo.custom;
 
 import lk.ijse.pos.bo.SuperBO;
+import lk.ijse.pos.dto.ItemDTO;
 import lk.ijse.pos.model.Item;
 
 import java.util.ArrayList;
 
 public interface ItemBO extends SuperBO {
-    public boolean addItem(Item item) throws Exception ;
+    public boolean addItem(ItemDTO item) throws Exception ;
 
     public boolean deleteItem(String code) throws Exception ;
 
-    public boolean updateItem(Item item) throws Exception ;
+    public boolean updateItem(ItemDTO item) throws Exception ;
 
     public boolean updateItemQtyOnHand(String code, int qtyOnHand)  throws Exception ;
 
-    public Item searchItem(String code) throws Exception ;
+    public ItemDTO searchItem(String code) throws Exception ;
 
-    public ArrayList<Item> getAllItems() throws Exception ;
+    public ArrayList<ItemDTO> getAllItems() throws Exception ;
 }
