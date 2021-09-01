25ac893d5a14d918bf093696ce78ffa1a36c5ded
Sandaru293 <skithshan93@gmail.com>
2021-09-01 18:07:26 +0530

    Added dto package & dtoclasses

--- POS-master/POS-master/POS/src/lk/ijse/pos/bo/impl/CustomerBOImpl.java
+++ POS-master/POS-master/POS/src/lk/ijse/pos/bo/impl/CustomerBOImpl.java
@@ -1,34 +1,19 @@
-package lk.ijse.pos.bo.impl;
+package lk.ijse.pos.bo.custom;
 
-import lk.ijse.pos.bo.custom.CustomerBO;
-import lk.ijse.pos.dao.DAOFactory;
-import lk.ijse.pos.dao.custom.CustomerDAO;
+import lk.ijse.pos.bo.SuperBO;
+import lk.ijse.pos.dto.CustomerDTO;
 import lk.ijse.pos.model.Customer;
 
 import java.util.ArrayList;
 
-public class CustomerBOImpl implements CustomerBO {
-    //private final CustomerDAO customerDAO = new CustomerDAOImpl();
-    private final CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+public interface CustomerBO extends SuperBO {
+    public boolean addCustomer(CustomerDTO customer) throws Exception ;
 
-    @Override
-    public boolean addCustomer(Customer customer) throws Exception {
-        return customerDAO.add(customer);
-    }
-    @Override
-    public boolean deleteCustomer(String id) throws Exception {
-        return customerDAO.delete(id);
-    }
-    @Override
-    public boolean updateCustomer(Customer customer) throws Exception {
-        return customerDAO.update(customer);
-    }
-    @Override
-    public Customer searchCustomer(String id) throws Exception {
-        return customerDAO.search(id);
-    }
-    @Override
-    public ArrayList<Customer> getAllCustomers() throws Exception {
-        return customerDAO.getAll();
-    }
+    public boolean deleteCustomer(String id) throws Exception ;
+
+    public boolean updateCustomer(CustomerDTO customer) throws Exception ;
+
+    public CustomerDTO searchCustomer(String id) throws Exception ;
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws Exception ;
 }
---
+++ POS-master/POS-master/POS/src/lk/ijse/pos/dto/OrderDetailsDTO.java
@@ -0,0 +1,63 @@
+package lk.ijse.pos.dto;
+
+import java.math.BigDecimal;
+
+public class OrderDetailsDTO {
+    private String orderId;
+    private String itemCode;
+    private int qty;
+    private BigDecimal unitPrice;
+
+    public OrderDetailsDTO() {
+    }
+
+    public OrderDetailsDTO(String orderId, String itemCode, int qty, BigDecimal unitPrice) {
+        this.orderId = orderId;
+        this.itemCode = itemCode;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("OrderDetailsDTO{");
+        sb.append("orderId='").append(orderId).append('\'');
+        sb.append(", itemCode='").append(itemCode).append('\'');
+        sb.append(", qty=").append(qty);
+        sb.append(", unitPrice=").append(unitPrice);
+        sb.append('}');
+        return sb.toString();
+    }
+}
